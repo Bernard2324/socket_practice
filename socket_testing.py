37698f837562fc26ceab42844e454770d3d83caa
Maurice Green <bernard.infosystems@gmail.com>
2017-11-03 16:24:09 -0500

    Add files via upload

---
+++ socket_testing.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import socket
+import time
+class httpconnect(object):
+
+	def __init__(self, addr, port=80, is_encrypted=False):
+		self.addr = addr
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.port = port
+		if is_encrypted and hasattr(self, 'port'):
+			setattr(self, 'port', 443)
+			try:
+				import ssl
+				self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+				setattr(getattr(self, 'context'), 'verify_mode', 1)
+				setattr(getattr(self, 'context'), 'check_hostname', True)
+				self.context.load_default_certs()
+			except ssl.SSLError:
+				raise AttributeError("Failed To Create SSL Context\n")
+			# wrap socket
+			assert hasattr(self, 'context') and hasattr(self, 'sock')
+			# increase timeout for ssl
+			self.sock.settimeout(10)
+			self.ssl_socket = self.context.wrap_socket(self.sock, server_hostname=addr)
+			self.ssl_socket.connect((self.addr, self.port))
+			self.instance = self.ssl_socket
+		else:
+			self.sock.connect((self.addr, self.port))
+			self.instance = self.sock
+
+	def Get(self, page='/'):
+		# the first response will always be the HTTP Server Response code, which is all we want.
+		request = "GET %s HTTP/1.0\r\n\r\n" % page
+		if hasattr(self, 'instance'):
+			self.instance.send(request)
+			while True:
+				return self.instance.recv(1024).split('\r')[0]
+
+
+if __name__ == "__main__":
+	test_servers = {
+		'www.google.com': 'enc', 'www.cnn.com': 'not_enc', 'www.iastate.edu': 'enc', 'www.facebook.com': 'enc',
+		'www.oreilly.com': 'enc', 'www.wikipedia.org': 'not_enc', 'www.fbi.gov': 'enc', 'www.cia.gov': 'enc',
+		'www.harvard.edu': 'enc', 'www.amazon.com': 'not_enc', 'www.twitter.com': 'enc','www.instagram.com': 'not_enc',
+		'www.ncaa.org': 'enc'
+	}
+	for uri in test_servers.keys():
+		start = time.time()
+		print "Testing URI: %s" % uri
+		try:
+			if not test_servers[uri] == 'enc':
+				test = httpconnect(uri)
+			else:
+				test = httpconnect(uri, is_encrypted=True)
+			print '\t'+test.Get()
+		except:
+			print "\t[*]Failed...."
+		print "\tElapsed Time: %s" % (time.time() - start)
